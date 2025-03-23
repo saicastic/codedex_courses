af602854a99f8a063e7d44bd740f65a5adc83e52
saicastic <saiprithvisuperior@gmail.com>
2025-03-23 11:15:36 +0530

    add initials

--- python/1 Hello World/4_Initials.py
+++ python/1 Hello World/4_Initials.py
@@ -1,1 +1,9 @@
 #  '#' is used for comments in python
+
+# FUN FACT - I won Snap Syntax Hackathon ( 1st position ) in Cognizance'25, IIT Roorkie
+
+print("S S S S")
+print("S      ")
+print("S S S S")
+print("      S")
+print("S S S S")
